388fb203246ac0faf6308e489acaf7bf54fb6812
Lisek <ognisty.lisek@gmail.com>
2015-01-04 17:50:18 +0000

    boxes now set size automatically to size of highest box

---
+++ tfgame/Scripts/boxesResize.js
@@ -0,0 +1,13 @@
+﻿boxes = $('.charaBox');
+maxHeight = Math.max.apply(
+Math, boxes.map(function () {
+    return $(this).height();
+}).get());
+boxes.height(maxHeight);
+
+boxes2 = $('.itemBox');
+maxHeight2 = Math.max.apply(
+Math, boxes2.map(function () {
+    return $(this).height();
+}).get());
+boxes2.height(maxHeight2);

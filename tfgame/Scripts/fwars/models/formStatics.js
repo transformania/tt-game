5b5204217d226ce80a3e48fce61831c90e145f6c
Judoo <elusivealbums@gmail.com>
2015-03-24 23:44:38 -0400

    broke up fwars into more files

---
+++ tfgame/Scripts/fwars/models/formStatics.js
@@ -0,0 +1,62 @@
+﻿// ----------------- FORM TYPE MAP ------------------
+var formStatsMap = {};
+
+formStatsMap['Female Bystander'] = {
+    'baseHP': 2,
+    'graphic': 'blurg.png',
+    'strengthAttack': 0,
+    'strengthDefense': 0,
+    'seductionAttack': 0,
+    'seductionDefense': 0,
+    'energyNeededToRecruit': 100,
+    'xpNeededForLevelup': 2,
+    'brainwashCost': 15,
+};
+
+formStatsMap['Male Bystander'] = {
+    'baseHP': 2,
+    'graphic': 'blurg.png',
+    'strengthAttack': 0,
+    'strengthDefense': 0,
+    'seductionAttack': 0,
+    'seductionDefense': 0,
+    'energyNeededToRecruit': 100,
+    'xpNeededForLevelup': 2,
+    'brainwashCost': 15,
+};
+
+formStatsMap['Fashion Witch'] = {
+    'baseHP': 50,
+    'graphic': 'blurg.png',
+    'strengthAttack': 1,
+    'strengthDefense': 0,
+    'seductionAttack': 3,
+    'seductionDefense': 0,
+    'energyNeededToRecruit': 10000,
+    'xpNeededForLevelup': 1,
+    'brainwashCost': 9999,
+};
+
+formStatsMap['Witchling'] = {
+    'baseHP': 4,
+    'graphic': 'blurg.png',
+    'strengthAttack': 1,
+    'strengthDefense': 0,
+    'seductionAttack': 2,
+    'seductionDefense': 0,
+    'energyNeededToRecruit': 200,
+    'xpNeededForLevelup': 5,
+    'brainwashCost': 35,
+};
+
+formStatsMap['Senior Witchling'] = {
+    'baseHP': 8,
+    'graphic': 'blurg.png',
+    'strengthAttack': 1,
+    'strengthDefense': 1,
+    'seductionAttack': 3,
+    'seductionDefense': 3,
+    'energyNeededToRecruit': 300,
+    'xpNeededForLevelup': 8,
+    'brainwashCost': 50,
+};
---
+++ tfgame/Scripts/fwars/models/itemStatics.js
@@ -0,0 +1,89 @@
+﻿// ----------------- ITEM TYPE MAP ------------------
+
+var itemStatsMap = {};
+
+itemStatsMap['Cotton Panties'] = {
+    'name': 'Cotton Panties',
+    'bonusHP': 3,
+    'value': 10,
+    'strengthAttack': 0,
+    'strengthDefense': 0,
+    'seductionAttack': 1,
+    'seductionDefense': 1,
+    'energyNeededToTF': 20,
+    'spellTrainingCost': 15,
+    'isSpell': true,
+    'isPurchaseableItem': true,
+    'Description': 'A pair of common pink panties.  Nothing too special or sexy.',
+};
+
+itemStatsMap['Latex Catsuit'] = {
+    'name': 'Latex Catsuit',
+    'bonusHP': 3,
+    'value': 15,
+    'strengthAttack': 0,
+    'strengthDefense': 0,
+    'seductionAttack': 1,
+    'seductionDefense': 1,
+    'energyNeededToTF': 25,
+    'spellTrainingCost': 25,
+    'isSpell': true,
+    'isPurchaseableItem': true,
+    'Description': 'A tight pink catsuit.  She squeaks a lot even when slightly touched; one can only imagine how she must feel when someone squeezes inside!',
+};
+
+itemStatsMap['Comfy Sports Bra'] = {
+    'name': 'Comfy Sports Bra',
+    'bonusHP': 4,
+    'value': 25,
+    'strengthAttack': 0,
+    'strengthDefense': 2,
+    'seductionAttack': 0,
+    'seductionDefense': 0,
+    'energyNeededToTF': 35,
+    'spellTrainingCost': 40,
+    'isSpell': true,
+    'isPurchaseableItem': true,
+    'Description': 'A comfortable sports bra, perfect for bustier witches who need to stay light on their feet.',
+};
+
+itemStatsMap['Lacy Corset'] = {
+    'name': 'Lacy Corset',
+    'bonusHP': -2,
+    'value': 40,
+    'strengthAttack': 0,
+    'strengthDefense': 0,
+    'seductionAttack': 5,
+    'seductionDefense': 5,
+    'energyNeededToTF': 50,
+    'spellTrainingCost': 40,
+    'isSpell': true,
+    'isPurchaseableItem': true,
+    'Description': 'A lacy violet corset, drawn tight around the waist with black strings.  A bit uncomfortable to wear but worth it... maybe.',
+};
+
+itemStatsMap['Ankh Necklace'] = {
+    'name': 'Ankh Necklace',
+    'bonusHP': 3,
+    'value': 25,
+    'strengthAttack': 0,
+    'strengthDefense': 0,
+    'seductionAttack': 1,
+    'seductionDefense': 1,
+    'energyNeededToTF': 50,
+    'spellTrainingCost': 200,
+    'isSpell': false,
+    'isPurchaseableItem': true,
+    'Description': 'An enchanted little amulet, boosting the power of whichever witch or wizard wearing it.',
+};
+
+
+itemStatsMap['Absorb'] = {
+    'name': 'Absorb',
+    'bonusHP': 0,
+    'value': 99999,
+    'energyNeededToTF': 25,
+    'spellTrainingCost': 25,
+    'isSpell': false,
+    'isPurchaseableItem': false,
+};

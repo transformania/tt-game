b9fff9d5deb6f4fbbba168623daa6ab0ed9ed2ab
Judoo <elusivealbums@gmail.com>
2015-04-28 18:55:27 -0400

    FW (and yeah this should be in a separate branch):  items have chance to get modifiers and added some properties to them

--- tfgame/Scripts/fwars/models/item.js
+++ tfgame/Scripts/fwars/models/item.js
@@ -27,10 +27,11 @@ ItemGroup.prototype.mergeIntoGroup = function (otherGroup) {
 /*
     type -- item type / name, ie 'Cotton Panties.'  Must match up with something in item statics
     oldName -- name of human who got turned into this.  Empty string if it is store bought or other
-    quality -- power modifier.  "default", ""
+    modifier -- modifier type, either Cute or Sexy for female and Rough / Classy for male.
+    modifierTier -- level of modifier
 */
 
-var Item = function (type, formerName, quality) {
+var Item = function (type, formerName) {
     this.type = type;
     this.id = itemIdNext;
 
@@ -40,12 +41,39 @@ var Item = function (type, formerName, quality) {
         this.oldName = formerName;
     }
 
-    if (typeof quality === undefined) {
-        this.quality = 'default';
+    this.modifier = "";
+    this.modifierTier = 0;
+
+    // make a roll to assign this item a modifier and tier
+    var targetGender = itemStatsMap[this.type].targetGender;
+    var roll = Math.random();
+
+    var WORSE = .04;
+    var BETTER = .14 + WORSE;
+
+    if (targetGender == "female") {
+        if (roll < WORSE) {
+            this.modifier = "sexy";
+        } else if (roll < BETTER) {
+            this.modifier = "cute";
+        }
     } else {
-        this.quality = quality;
+        if (roll < WORSE) {
+            this.modifier = "rough";
+        } else if (roll < BETTER) {
+            this.modifier = "classy";
+        }
     }
 
+    //if (roll < .18) {
+    //    var roll2 = Math.random();
+    //    if (roll2 < .01) {
+    //        this.modifierTier = 0;
+    //    } else if (roll2 < .01) {
+    //        this.modifierTier = 0;
+    //    }
+    //}
+    
     itemIdNext++;
 }
 
@@ -54,6 +82,28 @@ Item.prototype.getVictimName = function () {
         return '( ' + this.oldName + ' )';
     } else {
         return '';
+    }
+};
+
+Item.prototype.getModifier = function () {
+    if (this.modifier == '') {
+        return "";
+    } else {
+
+        // var output = "T" + this.modifierTier.toString() + " ";
+        var output = "";
+
+        if (this.modifier == "cute") {
+            output += "Cute";
+        } else if (this.modifier == "sexy") {
+            output += "Sexy";
+        } else if (this.modifier == "rough") {
+            output += "Rough";
+        } else if (this.modifier == "classy") {
+            output += "Classy";
+        }
+
+        return output;
     }
 };
 

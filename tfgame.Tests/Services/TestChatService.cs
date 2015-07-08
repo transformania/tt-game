6c50a9c02fccf0628b09cb2a26fa04bebee065f1
Andy Gibson <argibson86@gmail.com>
2015-07-08 02:23:46 +0100

    Expanded and tidied up the tests for marking player online activity in ChatService

--- tfgame.Tests/Services/TestChatService.cs
+++ tfgame.Tests/Services/TestChatService.cs
@@ -1,4 +1,5 @@
-﻿using NSubstitute;
+﻿using System;
+using NSubstitute;
 using NUnit.Framework;
 using tfgame.dbModels;
 using tfgame.dbModels.Abstract;
@@ -11,17 +12,40 @@ namespace tfgame.Tests.Services
     [TestFixture]
     public class TestChatService
     {
+        [SetUp]
+        public void SetUp()
+        {
+            DomainRegistry.Root = Substitute.For<IRoot>();
+        }
+        
         [Test]
         public void Should_update_player_online_activity_timestamp_if_overdue()
         {
-            var player = new Player_VM { Id = 12345 };
+            var player = new Player_VM 
+            { 
+                Id = 12345, 
+                OnlineActivityTimestamp = DateTime.UtcNow.AddMinutes(-3)
+            };
 
-            DomainRegistry.Root = Substitute.For<IRoot>();
             var chatService = new ChatService();
-
             chatService.MarkOnlineActivityTimestamp(player);
 
             DomainRegistry.Root.Received().Execute(Arg.Is<MarkOnlineActivityTimestamp>(cmd => cmd.Player.Id == player.Id));
+        }
+
+        [Test]
+        public void Should_not_update_player_online_activity_timestamp_if_not_overdue()
+        {
+            var player = new Player_VM
+            {
+                Id = 12345,
+                OnlineActivityTimestamp = DateTime.UtcNow.AddMinutes(-1)
+            };
+
+            var chatService = new ChatService();
+            chatService.MarkOnlineActivityTimestamp(player);
+
+            DomainRegistry.Root.DidNotReceive().Execute(Arg.Is<MarkOnlineActivityTimestamp>(cmd => cmd.Player.Id == player.Id));
         }
     }
 }

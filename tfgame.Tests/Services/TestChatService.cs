33b352076665cc795510140c6394c0bd75934b70
Andy Gibson <argibson86@gmail.com>
2015-07-08 01:50:36 +0100

    Added a new ChatService class to start encapsulating functionality in ChatHub. Also, first test

---
+++ tfgame.Tests/Services/TestChatService.cs
@@ -0,0 +1,27 @@
+﻿using NSubstitute;
+using NUnit.Framework;
+using tfgame.dbModels;
+using tfgame.dbModels.Abstract;
+using tfgame.dbModels.Commands.Player;
+using tfgame.dbModels.Models;
+using tfgame.Services;
+
+namespace tfgame.Tests.Services
+{
+    [TestFixture]
+    public class TestChatService
+    {
+        [Test]
+        public void Should_update_player_online_activity_timestamp_if_overdue()
+        {
+            var player = new Player_VM { Id = 12345 };
+
+            DomainRegistry.Root = Substitute.For<IRoot>();
+            var chatService = new ChatService();
+
+            chatService.MarkOnlineActivityTimestamp(player);
+
+            DomainRegistry.Root.Received().Execute(Arg.Is<MarkOnlineActivityTimestamp>(cmd => cmd.Player.Id == player.Id));
+        }
+    }
+}
---
+++ tfgame/Services/ChatService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using tfgame.dbModels;
+using tfgame.dbModels.Commands.Player;
+using tfgame.dbModels.Models;
+using tfgame.Statics;
+
+namespace tfgame.Services
+{
+    public class ChatService
+    {
+        private const int OnlineActivityCutoffMinutes = -2;
+        
+        public void MarkOnlineActivityTimestamp(Player_VM player)
+        {
+            var markOnlineCutoff = DateTime.UtcNow.AddMinutes(OnlineActivityCutoffMinutes);
+
+            // update the player's "last online" attribute if it's been long enough
+            if (player.OnlineActivityTimestamp >= markOnlineCutoff || PvPStatics.AnimateUpdateInProgress) 
+                return;
+
+            var cmd = new MarkOnlineActivityTimestamp {Player = player};
+            DomainRegistry.Root.Execute(cmd);
+        }
+    }
+}

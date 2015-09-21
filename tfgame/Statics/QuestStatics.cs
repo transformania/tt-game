ef2ae261784b9e0716ed6b58b0060432eead3f0a
Judoo <elusivealbums@gmail.com>
2015-09-21 12:58:05 -0400

    enums for operator and requirement types

--- tfgame/Statics/QuestStatics.cs
+++ tfgame/Statics/QuestStatics.cs
@@ -10,5 +10,29 @@ namespace tfgame.Statics
         public enum QuestOutcomes { Failed = 0, Completed = 1 };
 
         public enum Gender { Male = 0, Female = 1, Any = 2 };
+
+        public enum Operator {
+            Less_Than = 0,
+            Less_Than_Or_Equal = 1,
+            Equal_To = 2,
+            Greater_Than_Or_Equal = 3,
+            Greater_Than = 4,
+            Not_Equal_To = 5
+        };
+
+        public enum RequirementType {
+            Discipline = 0,
+            Perception = 1,
+            Charisma = 2,
+            Fortitude = 3,
+            Agility = 4,
+            Allure = 5,
+            Magicka = 6,
+            Succour = 7,
+            Luck = 8, 
+            Variable = 9
+        }
+
+
     }
 }

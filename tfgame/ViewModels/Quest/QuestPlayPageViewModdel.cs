86a84f2125d20c2f7b42f886345e4051e218cb4f
Judoo <elusivealbums@gmail.com>
2015-09-28 16:04:07 -0400

    More quest updates

---
+++ tfgame/ViewModels/Quest/QuestPlayPageViewModdel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using tfgame.dbModels.Models;
+
+namespace tfgame.ViewModels.Quest
+{
+    public class QuestPlayPageViewModdel
+    {
+        public QuestStart QuestStart { get; set; }
+        public QuestState QuestState { get; set; }
+        public PlayerFormViewModel Player { get; set; }
+    }
+}

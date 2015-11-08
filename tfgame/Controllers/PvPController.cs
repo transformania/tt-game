6312c2d0304bf023f4c08809272abb0b9d167659
Judoo <elusivealbums@gmail.com>
2015-11-08 00:14:22 -0500

    A new way of polling for turn updates, rather than having users have a chance of triggering it (sometimes more than one at a time on rare occasions) the server has a timer created on startup that repeatedly polls every 30 seconds for an update.

--- tfgame/Controllers/PvPController.cs
+++ tfgame/Controllers/PvPController.cs
@@ -93,11 +93,11 @@ namespace tfgame.Controllers
             double secondsSinceUpdate = Math.Abs(Math.Floor(WorldStat.LastUpdateTimestamp.Subtract(DateTime.UtcNow).TotalSeconds));
             ViewBag.SecondsUntilUpdate = PvPStatics.TurnSecondLength-(int)secondsSinceUpdate;
 
-            // if it has been long enough since last update, force an update to occur
-            if (secondsSinceUpdate > PvPStatics.TurnSecondLength && WorldStat.WorldIsUpdating == false && PvPStatics.AnimateUpdateInProgress == false)
-            {
-                UpdateWorld();
-            }
+            //// if it has been long enough since last update, force an update to occur
+            //if (secondsSinceUpdate > PvPStatics.TurnSecondLength && WorldStat.WorldIsUpdating == false && PvPStatics.AnimateUpdateInProgress == false)
+            //{
+            //    UpdateWorld();
+            //}
 
             // turn off world update toggle if it's simply been too long
             if (secondsSinceUpdate > 90 && (PvPStatics.AnimateUpdateInProgress == true || WorldStat.WorldIsUpdating == true))

a33bde99e7c3b117fa0c66829d1e8b652a4a00d3
Andy Gibson <argibson86@gmail.com>
2016-02-15 20:05:47 +0000

    Dropped the constructor on CreateChatRoom to allow dynamic creation for the API and added a check to ensure the creator of a chat room exists.

--- src/TT.Domain/Commands/Chat/CreateChatRoom.cs
+++ src/TT.Domain/Commands/Chat/CreateChatRoom.cs
@@ -8,14 +8,8 @@ namespace TT.Domain.Commands.Chat
 {
     public class CreateChatRoom : DomainCommand<ChatRoom>
     {
-        public string RoomName { get; private set; }
-        public string CreatorId { get; private set; }
-
-        public CreateChatRoom(string roomName, string creatorId)
-        {
-            RoomName = roomName;
-            CreatorId = creatorId;
-        }
+        public string RoomName { get; set; }
+        public string CreatorId { get; set; }
 
         public override ChatRoom Execute(IDataContext context)
         {
@@ -28,7 +22,9 @@ namespace TT.Domain.Commands.Chat
                 if (ctx.AsQueryable<ChatRoom>().Any(cr => cr.Name == RoomName))
                     throw new DomainException("Chat room '{0}' already exists", RoomName);
 
-                var creator = ctx.AsQueryable<User>().Single(u => u.Id == CreatorId);
+                var creator = ctx.AsQueryable<User>().SingleOrDefault(u => u.Id == CreatorId);
+                if (creator == null)
+                    throw new DomainException("Room creator does not exist");
 
                 room = ChatRoom.Create(creator, RoomName);
 
@@ -43,6 +39,12 @@ namespace TT.Domain.Commands.Chat
 
         private void Validate()
         {
+            if (string.IsNullOrWhiteSpace(RoomName))
+                throw new DomainException("No room name was provided");
+
+            if (string.IsNullOrWhiteSpace(CreatorId))
+                throw new DomainException("No room creator was provided");
+
             var regex = new Regex("^[a-zA-Z0-9_-]*$");
             if (!regex.IsMatch(RoomName))
                 throw new DomainException("Chat room '{0}' contains unsupported characters, only alphanumeric names with _ or - are allowed",RoomName);
--- src/TT.Tests/Domain/Commands/Chat/CreateChatRoomTests.cs
+++ src/TT.Tests/Domain/Commands/Chat/CreateChatRoomTests.cs
@@ -18,7 +18,7 @@ namespace TT.Tests.Domain.Commands.Chat
         {
             var creator = new UserBuilder().BuildAndSave();
             
-            var cmd = new CreateChatRoom("Test_Room", creator.Id);
+            var cmd = new CreateChatRoom { RoomName = "Test_Room", CreatorId = creator.Id };
 
             var room = Repository.Execute(cmd);
 
@@ -37,7 +37,7 @@ namespace TT.Tests.Domain.Commands.Chat
 
             new ChatRoomBuilder().With(cr => cr.Name, existingName).BuildAndSave();
 
-            var cmd = new CreateChatRoom(existingName, Guid.NewGuid().ToString());
+            var cmd = new CreateChatRoom { RoomName = existingName, CreatorId = Guid.NewGuid().ToString() };
 
             var action = new Action(() => { Repository.Execute(cmd); });
 
@@ -53,7 +53,7 @@ namespace TT.Tests.Domain.Commands.Chat
         [TestCase("Test%")]
         public void Should_not_allow_special_characters_in_room_name(string roomName)
         {
-            var cmd = new CreateChatRoom(roomName, Guid.NewGuid().ToString());
+            var cmd = new CreateChatRoom { RoomName = roomName, CreatorId = Guid.NewGuid().ToString() };
 
             var action = new Action(() => { Repository.Execute(cmd); });
 
@@ -74,9 +74,39 @@ namespace TT.Tests.Domain.Commands.Chat
         {
             var creator = new UserBuilder().BuildAndSave();
 
-            Repository.Execute(new CreateChatRoom(roomName, creator.Id));
+            Repository.Execute(new CreateChatRoom { RoomName = roomName, CreatorId = creator.Id});
 
             DataContext.AsQueryable<ChatRoom>().Any(cr => cr.Name == roomName && cr.Creator.Id == creator.Id).Should().BeTrue();
+        }
+
+        [Test]
+        public void Should_require_a_room_name_to_be_provided()
+        {
+            var cmd = new CreateChatRoom { CreatorId = Guid.NewGuid().ToString() };
+
+            var action = new Action(() => { Repository.Execute(cmd); });
+
+            action.ShouldThrowExactly<DomainException>().WithMessage("No room name was provided");
+        }
+
+        [Test]
+        public void Should_require_a_creator_id()
+        {
+            var cmd = new CreateChatRoom { RoomName = "Test_Room" };
+
+            var action = new Action(() => { Repository.Execute(cmd); });
+
+            action.ShouldThrowExactly<DomainException>().WithMessage("No room creator was provided");
+        }
+
+        [Test]
+        public void Should_not_be_able_to_create_room_if_creator_does_not_exist()
+        {
+            var cmd = new CreateChatRoom { RoomName = "Test_Room", CreatorId = Guid.NewGuid().ToString()};
+
+            var action = new Action(() => { Repository.Execute(cmd); });
+
+            action.ShouldThrowExactly<DomainException>().WithMessage("Room creator does not exist");
         }
     }
 }

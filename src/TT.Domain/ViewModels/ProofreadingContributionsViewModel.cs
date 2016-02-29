a8620dbeefb4b4c6fe00f3f9ad606f9bbe974da1
Arrhae Khellian <arrhae_tkhellian@yahoo.com>
2016-02-29 06:37:39 -0800

    Make contributions list pull from JSON. Fixes issue #264.

---
+++ src/TT.Domain/ViewModels/ProofreadingContributionsViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace TT.Domain.ViewModels
+{
+    public class ProofreadingContributionsViewModel
+    {
+        public int Id { get; set; }
+        public bool IsLive { get; set; }
+        public bool ProofreadingCopy { get; set; }
+        public bool ProofreadingLockIsOn { get; set; }
+        public string CheckedOutBy { get; set; }
+        public DateTime CreationTimestamp { get; set; }
+        public string Skill_FriendlyName { get; set; }
+        public string Form_FriendlyName { get; set; }
+        public bool NeedsToBeUpdated { get; set; }
+    }
+}

8ff2965c3cc2cb0552ac838424429833e293ecdc
Judoo <maltorvHA@gmail.com>
2016-03-30 21:34:53 -0400

    Converting the tome read code to load from the Tome table of the database instead of reading XML files.  Removing said XML files.

---
+++ src/TT.Domain/Queries/Assets/GetTomeByItem.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+using TT.Domain.DTOs.Assets;
+using TT.Domain.Entities.Assets;
+
+namespace TT.Domain.Queries.Assets
+{
+    public class GetTomeByItem : Highway.Data.Scalar<TomeDetail>
+    {
+        public GetTomeByItem(int id)
+        {
+            ContextQuery = ctx =>
+            {
+                return ctx.AsQueryable<Tome>()
+                            .Where(cr => cr.BaseItem.Id == id)
+                            .ProjectToFirstOrDefault<TomeDetail>();
+            };
+        }
+    }
+}
---
+++ src/TT.Tests/Domain/Queries/Assets/GetTomeByItemTest.cs
@@ -0,0 +1,31 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using TT.Domain;
+using TT.Domain.Queries.Assets;
+using TT.Tests.Builders.Assets;
+using TT.Tests.Builders.Item;
+
+namespace TT.Tests.Domain.Queries.Assets
+{
+    [TestFixture]
+    public class GetTomeByItemTests : TestBase
+    {
+        [Test]
+        public void Should_fetch_tome_by_item_id()
+        {
+            new TomeBuilder().With(cr => cr.Id, 7)
+                .With(cr => cr.Text, "First Tome")
+                .With(cr => cr.BaseItem, new ItemBuilder().With(cr => cr.Id, 195).BuildAndSave())
+                .BuildAndSave();
+
+            var cmd = new GetTomeByItem(195);
+
+            var tome = DomainRegistry.Repository.Find(cmd);
+
+            tome.Id.Should().Equals(7);
+            tome.Text.Should().BeEquivalentTo("First Tome");
+            tome.BaseItem.Id.Should().Equals(195);
+
+        }
+    }
+}

f48cb7b2c8de045622be8d6e211db653d4ae8ed2
Andy Gibson <argibson86@gmail.com>
2016-04-01 16:48:59 +0100

    Moved feature switches around to allow access to them in the domain

--- src/TT.Domain/Features.cs
+++ src/TT.Domain/Features.cs
@@ -1,8 +1,11 @@
 ﻿using FeatureSwitch;
 using FeatureSwitch.Strategies;
 
-namespace TT.Web
+namespace TT.Domain
 {
     [AppSettings(Key = "Features.Chat.V2")]
     public class ChatV2 : BaseFeature {}
+
+    [AppSettings(Key = "Features.Misc.AprilFools2016")]
+    public class AprilFools2016 : BaseFeature { }
 }

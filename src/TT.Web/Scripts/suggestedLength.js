47cab6b326dee40ccb192128ea5d6e77c1f1323d
Judoo <maltorvHA@gmail.com>
2016-04-17 20:24:36 -0400

    Refactored a bit, pulling out into a separate script and removing some duplicate code

---
+++ src/TT.Web/Scripts/suggestedLength.js
@@ -0,0 +1,43 @@
+﻿/*
+    This script will create a <div> after any dom element with the the attribute 'SuggestedLength'.  Some text will be rendered beneath
+    the element that states if the desired character length has been met where desired length is the value set for the attribute.
+*/
+
+$(document).ready(function () {
+    bindSuggestedLengths();
+});
+
+function bindSuggestedLengths() {
+
+    $("[suggestedlength]").each(function () {
+        var desiredLength = $(this).attr("suggestedlength");
+        var box = $(this);
+        box.after("<div style='text-align: right;'></div>");
+        setText(box, desiredLength);
+
+        $(this).on('input', function () {
+            setText($(this), desiredLength);
+        });
+    });
+
+    function setText(box, desiredLength) {
+
+        var note = box.next();
+        var currentLength = box.val().length;
+
+        var classToUse = "bad";
+
+        if (currentLength >= desiredLength) {
+            classToUse = "good";
+        }
+
+        note.removeClass("good");
+        note.removeClass("bad");
+        note.addClass(classToUse);
+
+        note.html(currentLength + "/" + desiredLength + " of minimum suggested character length");
+
+    }
+
+}
+

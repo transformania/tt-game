3089f36fa40673d734ddaeab7a90da08854476d2
Judoo <maltorvHA@gmail.com>
2016-04-24 16:05:15 -0400

    Fixing bug when talking to Jewdewfae

---
+++ src/TT.Domain/ViewModels/NPCs/JewdewfaeEncounterViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TT.Domain.Models;
+
+namespace TT.Domain.ViewModels.NPCs
+{
+    public class JewdewfaeEncounterViewModel
+    {
+        public JewdewfaeEncounter JewdewfaeEncounter { get; set; }
+        public bool IsInWrongForm { get; set; }
+        public bool IsTired { get; set; }
+        public bool ShowSuccess { get; set; }
+        public bool HadRecentInteraction { get; set; }
+        public decimal XPGain { get; set; }
+        public string SpellsLearned { get; set; }
+    }
+}

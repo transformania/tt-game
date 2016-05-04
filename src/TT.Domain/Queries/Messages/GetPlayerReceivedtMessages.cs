0309b2505b14631fc033e39296b976bfb9d7ad24
Judoo <maltorvHA@gmail.com>
2016-05-03 22:44:19 -0400

    Added queries and tests for them to grab all of a player's sent and received messages

---
+++ src/TT.Domain/Queries/Messages/GetPlayerReceivedtMessages.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Highway.Data;
+using TT.Domain.DTOs.Messages;
+using TT.Domain.Entities.Messages;
+
+namespace TT.Domain.Queries.Messages
+{
+    public class GetPlayerReceivedMessages : DomainQuery<MessageDetail>
+    {
+
+        public int ReceiverId { get; set; }
+
+        public override IEnumerable<MessageDetail> Execute(IDataContext context)
+        {
+            ContextQuery = ctx => ctx.AsQueryable<Message>().ProjectToQueryable<MessageDetail>().Where(m => m.Receiver.Id == ReceiverId);
+            return ExecuteInternal(context);
+        }
+    }
+}
--- src/TT.Tests/Domain/Queries/Messages/GetMessagesTests.cs
+++ src/TT.Tests/Domain/Queries/Messages/GetMessagesTests.cs
@@ -1,7 +1,9 @@
-﻿using FluentAssertions;
+﻿using System.Linq;
+using FluentAssertions;
 using NUnit.Framework;
 using TT.Domain;
 using TT.Domain.Queries.Messages;
+using TT.Tests.Builders.Item;
 using TT.Tests.Builders.Messages;
 
 namespace TT.Tests.Domain.Queries.Messages
@@ -30,17 +32,73 @@ namespace TT.Tests.Domain.Queries.Messages
         }
 
         [Test]
-        [Ignore("TODO")]
         public void Should_fetch_all_players_received_messages()
         {
-            
+
+            var sender = new PlayerBuilder().With(p => p.Id, 25)
+                .With(p => p.FirstName, "Tom")
+                .BuildAndSave();
+
+            var receiver = new PlayerBuilder().With(p => p.Id, 78)
+               .With(p => p.FirstName, "Carl").BuildAndSave();
+
+            new MessageBuilder().With(m => m.Id, 23)
+                .With(p => p.Sender, sender)
+                .With(p => p.Receiver, receiver)
+                .With(p => p.MessageText, "hello")
+               .BuildAndSave();
+
+            new MessageBuilder().With(m => m.Id, 23)
+                .With(p => p.Sender, sender)
+                .With(p => p.Receiver, receiver)
+                .With(p => p.MessageText, "world!")
+               .BuildAndSave();
+
+            var cmd = new GetPlayerSentMessages
+            {
+                SenderId = sender.Id
+            };
+
+            var messages = DomainRegistry.Repository.Find(cmd);
+
+            messages.Should().HaveCount(2);
+            messages.First().MessageText.Should().BeEquivalentTo("hello");
+            messages.Last().MessageText.Should().BeEquivalentTo("world!");
+
         }
 
         [Test]
-        [Ignore("TODO")]
-        public void Should_fetch_all_players_sent_messages()
+        public void Should_fetch_all_players_receiver_messages()
         {
+            var sender = new PlayerBuilder().With(p => p.Id, 25)
+                .With(p => p.FirstName, "Tom")
+                .BuildAndSave();
 
+            var receiver = new PlayerBuilder().With(p => p.Id, 78)
+               .With(p => p.FirstName, "Carl").BuildAndSave();
+
+            new MessageBuilder().With(m => m.Id, 23)
+                .With(p => p.Sender, sender)
+                .With(p => p.Receiver, receiver)
+                .With(p => p.MessageText, "hello")
+               .BuildAndSave();
+
+            new MessageBuilder().With(m => m.Id, 23)
+                .With(p => p.Sender, sender)
+                .With(p => p.Receiver, receiver)
+                .With(p => p.MessageText, "world!")
+               .BuildAndSave();
+
+            var cmd = new GetPlayerReceivedMessages
+            {
+                ReceiverId = receiver.Id
+            };
+
+            var messages = DomainRegistry.Repository.Find(cmd);
+
+            messages.Should().HaveCount(2);
+            messages.First().MessageText.Should().BeEquivalentTo("hello");
+            messages.Last().MessageText.Should().BeEquivalentTo("world!");
         }
 
 

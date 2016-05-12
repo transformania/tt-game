63da09403efa66a0aca332c8a8ed7e03af0f1ced
Arrhae Khellian <arrhae_tkhellian@yahoo.com>
2016-05-12 16:06:00 -0700

    Update Item migration to null OwnerIds set to -1.

--- src/TT.Migrations/201605011120_AddItemFKs.cs
+++ src/TT.Migrations/201605011120_AddItemFKs.cs
@@ -9,14 +9,14 @@ namespace TT.Migrations
         
         public override void Up()
         {
+            Alter.Table("Items").AlterColumn("OwnerId").AsInt32().Nullable();
+            Update.Table("Items").Set(new { OwnerId = null as object }).Where(new { OwnerId = -1 });
 
             Create.ForeignKey()
                 .FromTable("Items")
                 .ForeignColumn("OwnerId")
                 .ToTable("Players")
                 .PrimaryColumn("Id").OnDelete(Rule.None);
-
-            Alter.Table("Items").AlterColumn("OwnerId").AsInt32().Nullable();
 
             Alter.Table("Items").AddColumn("ItemSourceId").AsInt32().ForeignKey("DbStaticItems", "Id");
 

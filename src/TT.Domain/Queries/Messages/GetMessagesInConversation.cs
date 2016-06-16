9dfa9d24f94296092475289b4291b1173cf32ebe
Judoo <maltorvHA@gmail.com>
2016-06-15 22:54:03 -0400

    Added ability to view all messages in a conversation via an Ajax call

---
+++ src/TT.Domain/Queries/Messages/GetMessagesInConversation.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Highway.Data;
+using TT.Domain.DTOs.Messages;
+using TT.Domain.Entities.Messages;
+
+namespace TT.Domain.Queries.Messages
+{
+    public class GetMessagesInConversation : DomainQuery<MessageDetail>
+    {
+
+        public Guid? conversationId { get; set; }
+
+        public override IEnumerable<MessageDetail> Execute(IDataContext context)
+        {
+
+            ContextQuery = ctx => ctx.AsQueryable<Message>().Where(m => m.ConversationId == conversationId).ProjectToQueryable<MessageDetail>().OrderByDescending(m => m.Timestamp);
+            return ExecuteInternal(context);
+        }
+
+        protected override void Validate()
+        {
+            if (conversationId == null)
+                throw new DomainException("ConversationId cannot be null");
+        }
+    }
+}
---
+++ src/TT.Tests/Domain/Queries/Messages/GetMEssagesInConversationTests.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Linq;
+using FluentAssertions;
+using NUnit.Framework;
+using TT.Domain;
+using TT.Domain.Queries.Messages;
+using TT.Tests.Builders.Item;
+using TT.Tests.Builders.Messages;
+
+namespace TT.Tests.Domain.Queries.Messages
+{
+    [TestFixture]
+    public class GetMessagesInConversationTests : TestBase
+    {
+
+        [Test]
+        public void should_get_messages_in_conversation()
+        {
+            var receiver = new PlayerBuilder().With(p => p.Id, 25)
+                  .BuildAndSave();
+
+            var otherPlayer = new PlayerBuilder().With(p => p.Id, 30)
+               .BuildAndSave();
+
+            var guid1 = new Guid("0cd4537b-f6b3-4cab-ae18-83a880c6d070");
+            var guid2 = new Guid("11309252-9094-45a2-945a-85cd882d4a2e");
+
+            new MessageBuilder().With(m => m.Id, 1)
+                    .With(m => m.Receiver, receiver)
+                    .With(m => m.ConversationId, guid1)
+                    .BuildAndSave();
+
+            new MessageBuilder().With(m => m.Id, 2)
+                    .With(m => m.Receiver, receiver)
+                    .With(m => m.ConversationId, guid1)
+                    .BuildAndSave();
+
+            new MessageBuilder().With(m => m.Id, 3)
+                    .With(m => m.Receiver, receiver)
+                    .With(m => m.ConversationId, guid2)
+                    .BuildAndSave();
+
+            new MessageBuilder().With(m => m.Id, 4)
+                    .With(m => m.Receiver, otherPlayer)
+                    .With(m => m.ConversationId, guid1)
+                    .BuildAndSave();
+
+            var cmd = new GetMessagesInConversation { conversationId = guid1};
+            var messages = DomainRegistry.Repository.Find(cmd);
+
+            var ids = messages.Select(m => m.Id);
+
+            ids.Should().Contain(1);
+            ids.Should().Contain(2);
+            ids.Should().Contain(4);
+            ids.Should().NotContain(3);
+        }
+
+        [Test]
+        public void should_throw_exception_if_conversationId_is_null()
+        {
+            var cmd = new GetMessagesInConversation { conversationId = null };
+            var action = new Action(() => { Repository.Find(cmd); });
+
+            action.ShouldThrowExactly<DomainException>().WithMessage("ConversationId cannot be null");
+        }
+
+        
+    }
+}

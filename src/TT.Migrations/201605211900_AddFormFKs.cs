dff57c081294bd39c240ffa1a4fd5c283aa3439d
Judoo <maltorvHA@gmail.com>
2016-06-22 08:20:36 -0400

    Fixed migration.  Because '0' is not a valid FK.  Deeeerp.

--- src/TT.Migrations/201605211900_AddFormFKs.cs
+++ src/TT.Migrations/201605211900_AddFormFKs.cs
@@ -9,8 +9,8 @@ namespace TT.Migrations
         
         public override void Up()
         {
-            Alter.Table("Players").AlterColumn("FormSourceId").AsInt32().ForeignKey("DbStaticForms", "Id");
             Execute.Sql("UPDATE Players SET FormSourceId = DbStaticForms.Id FROM DbStaticForms WHERE DbStaticForms.dbName = Players.Form");
+            Alter.Table("Players").AlterColumn("FormSourceId").AsInt32().ForeignKey("DbStaticForms", "Id");
         }
 
     }

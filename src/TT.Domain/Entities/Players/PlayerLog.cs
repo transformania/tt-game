eebe5b3ef1b428b39765ea786417e4ff07090dcd
Judoo <maltorvHA@gmail.com>
2016-06-24 13:40:56 -0400

    Adding PlayerLog Entity, mapping, and migrations

---
+++ src/TT.Domain/Entities/Players/PlayerLog.cs
@@ -0,0 +1,16 @@
+﻿
+using System;
+
+namespace TT.Domain.Entities.Players
+{
+    public class PlayerLog : Entity<int>
+    {
+      
+        public Player Owner { get; protected set; }
+        public string Message { get; protected set; }
+        public DateTime Timestamp { get; protected set; }
+        public bool IsImportant { get; protected set; }
+
+        private PlayerLog() { }
+    }
+}

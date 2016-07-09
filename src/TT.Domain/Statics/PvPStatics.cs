e7d598f6963c5a868bc6a0ba6d39fdd713354682
Judoo <maltorvHA@gmail.com>
2016-07-08 20:06:20 -0400

    Changed round number to alpha 35 and updated reset world script to mention TRUNCATE of db tables

--- src/TT.Domain/Statics/PvPStatics.cs
+++ src/TT.Domain/Statics/PvPStatics.cs
@@ -18,7 +18,7 @@ namespace TT.Domain.Statics
         public const int StartTurnNoAttackSeconds = 30;
         public const int EndTurnNoAttackSeconds = 30;
 
-        public const string AlphaRound = "Alpha Round 34";
+        public const string AlphaRound = "Alpha Round 35";
 
         public static bool ChaosMode = false;
 

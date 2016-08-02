9b5d71be561cc53dc2ab14f420f6eaffcbba7393
Judoo <maltorvHA@gmail.com>
2016-08-02 14:33:26 -0400

    Fixing TFEnergy migration.  Currently it can error out when trying to migrate TF energies owned by a Player that does not exist; those are now deleted before FK is set.  Also added 72 hour autodelete policy on TFenergy.

--- src/TT.Migrations/201607151721_AddTFEnergyFK.cs
+++ src/TT.Migrations/201607151721_AddTFEnergyFK.cs
@@ -10,13 +10,15 @@ namespace TT.Migrations
         public override void Up()
         {
 
+            Execute.Sql("DELETE [dbo].[TFEnergies] FROM[dbo].[TFEnergies] LEFT OUTER JOIN Players ON TFEnergies.PlayerId = Players.Id WHERE TFEnergies.PlayerId != -1 AND Players.Id IS NULL");
+
+            Execute.Sql("DELETE [dbo].[TFEnergies] FROM[dbo].[TFEnergies] LEFT OUTER JOIN Players ON TFEnergies.CasterId = Players.Id WHERE TFEnergies.CasterId != -1 AND Players.Id IS NULL");
+
             Create.ForeignKey()
                 .FromTable("TFEnergies")
                 .ForeignColumn("PlayerId")
                 .ToTable("Players")
                 .PrimaryColumn("Id").OnDelete(Rule.None);
-
-            Execute.Sql("DELETE [dbo].[TFEnergies] FROM[dbo].[TFEnergies] LEFT OUTER JOIN Players ON TFEnergies.CasterId = Players.Id WHERE TFEnergies.CasterId != -1 AND Players.Id IS NULL");
 
             Update.Table("TFEnergies").Set(new { CasterId = null as object }).Where(new { CasterId = -1 });
             Alter.Table("TFEnergies").AlterColumn("CasterId").AsInt32().Nullable().ForeignKey("Players", "Id");

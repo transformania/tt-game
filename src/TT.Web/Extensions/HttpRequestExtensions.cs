7bfe7613a03bab21ed81eff8adc57460466e5763
Arrhae Khellian <arrhae_tkhellian@yahoo.com>
2016-11-29 11:51:46 -0800

    Add support for detecting IP address when behind Cloudflare. Fixes issue #397.

---
+++ src/TT.Web/Extensions/HttpRequestExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System.Web;
+using FeatureSwitch;
+using TT.Domain;
+
+namespace TT.Web.Extensions
+{
+    public static class HttpRequestExtensions
+    {
+        public static string GetRealUserHostAddress(this HttpRequestBase request)
+        {
+            return FeatureContext.IsEnabled<UseCloudflare>() &&
+                   !string.IsNullOrEmpty(request.ServerVariables["HTTP_CF_CONNECTING_IP"])
+                ? request.ServerVariables["HTTP_CF_CONNECTING_IP"]
+                : request.UserHostAddress;
+        }
+    }
+}

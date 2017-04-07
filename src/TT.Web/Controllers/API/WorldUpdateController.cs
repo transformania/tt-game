0b33ffcb73fd9ab01f5360db3698ceeb4557ed0d
Judoo <maltorvHA@gmail.com>
2017-04-06 23:05:36 -0400

    Save the round leaderboards on the final turn of the round.  The commands will throw an exception if try to re-save a leaderboard.

--- src/TT.Web/Controllers/API/WorldUpdateController.cs
+++ src/TT.Web/Controllers/API/WorldUpdateController.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Web.Http;
+using TT.Domain;
 using TT.Domain.Abstract;
 using TT.Domain.Concrete;
+using TT.Domain.Exceptions;
 using TT.Domain.Procedures;
 using TT.Domain.Statics;
+using TT.Domain.World.Commands;
 
 namespace TT.Web.Controllers.API
 {
@@ -25,6 +28,43 @@ namespace TT.Web.Controllers.API
 
             IPvPWorldStatRepository worldStatRepo = new EFPvPWorldStatRepository();
             var world = worldStatRepo.PvPWorldStats.First();
+
+            if (world.TurnNumber == world.RoundDuration && !world.ChaosMode)
+            {
+                var round = Int32.Parse(PvPStatics.AlphaRound.Split(' ')[2]);
+                var errorSavingLeaderboards = false;
+                try
+                {
+                    DomainRegistry.Repository.Execute(new SavePvPLeaderboards { RoundNumber = round }); 
+                }
+                catch (DomainException e)
+                {
+                    errorSavingLeaderboards = true;
+                }
+
+                try
+                {
+                    DomainRegistry.Repository.Execute(new SaveXPLeaderboards { RoundNumber = round });
+                }
+                catch (DomainException e)
+                {
+                    errorSavingLeaderboards = true;
+                }
+
+                try
+                {
+                    DomainRegistry.Repository.Execute(new SaveItemLeaderboards { RoundNumber = round });
+                }
+                catch (DomainException e)
+                {
+                    errorSavingLeaderboards = true;
+                }
+
+                if (errorSavingLeaderboards)
+                {
+                    return InternalServerError();
+                }
+            }
 
             // Don't do a turn update if the round is over or we're still in an update
             if (world.TurnNumber >= PvPStatics.RoundDuration || world.WorldIsUpdating) return BadRequest();

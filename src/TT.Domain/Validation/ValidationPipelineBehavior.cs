4c3cf991ad00df8b6445418ab2e065942daf36c2
Eric Force <augustustate94@gmail.com>
2017-05-16 03:40:38 -0500

    Created the registration needed for MediatR and FluentValidation. Created a pipeline that will validate the request before it is handled.

---
+++ src/TT.Domain/Validation/ValidationPipelineBehavior.cs
@@ -0,0 +1,34 @@
+﻿using FluentValidation;
+using MediatR;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TT.Domain.Validation
+{
+    public class ValidationPipelineBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    {
+        private readonly IValidator<TRequest>[] validators;
+
+        public ValidationPipelineBehavior(IValidator<TRequest>[] validators)
+        {
+            this.validators = validators;
+        }
+
+        public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next)
+        {
+            var validationContext = new ValidationContext<TRequest>(request);
+
+            var failures = validators.Select(validator => validator.Validate(validationContext))
+                .SelectMany(r => r.Errors)
+                .Where(f => f != null)
+                .ToList();
+
+            if (failures.Any())
+            {
+                throw new ValidationException(failures);
+            }
+
+            return await next();
+        }
+    }
+}
--- src/TT.Web/App_Start/ContainerConfig.cs
+++ src/TT.Web/App_Start/ContainerConfig.cs
@@ -16,6 +16,10 @@ using TT.Web.Models;
 using Owin;
 using Microsoft.Owin.Security.DataProtection;
 using Microsoft.AspNet.Identity.Owin;
+using MediatR;
+using TT.Domain;
+using FluentValidation;
+using TT.Domain.Validation;
 
 namespace TT.Web
 {
@@ -24,6 +28,9 @@ namespace TT.Web
         public static void ConfigureContainer(HttpConfiguration httpConfig, IAppBuilder app)
         {
             var container = new Container();
+            var webAssembly = typeof(ContainerConfig).Assembly;
+            var domainAssembly = typeof(DomainContext).Assembly;
+
             container.Options.DefaultScopedLifestyle = new WebRequestLifestyle();
 
             container.Register(() =>
@@ -43,8 +50,23 @@ namespace TT.Web
                 ? new OwinContext(new Dictionary<string, object>()).Authentication
                 : HttpContext.Current.GetOwinContext().Authentication, Lifestyle.Scoped);
 
-            container.RegisterMvcControllers(Assembly.GetExecutingAssembly());
+            container.RegisterMvcControllers(webAssembly);
             container.RegisterWebApiControllers(httpConfig);
+
+            // Mediator
+            container.RegisterSingleton<IMediator, Mediator>();
+            container.RegisterSingleton(new SingleInstanceFactory(container.GetInstance));
+            container.RegisterSingleton(new MultiInstanceFactory(container.GetAllInstances));
+
+            // Request Handler
+            container.Register(typeof(IRequestHandler<,>), new []{ domainAssembly });
+            container.Register(typeof(IRequestHandler<>), new []{ domainAssembly });
+
+            // Pipeline
+            container.RegisterCollection(typeof(IPipelineBehavior<,>), domainAssembly);
+
+            // Validator
+            container.RegisterCollection(typeof(IValidator<>), domainAssembly);
 
             container.Verify();
 

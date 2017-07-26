6f59ea35956aee30f400427705e85578d052504b
Eric Force <augustustate94@gmail.com>
2017-07-26 14:26:12 -0500

    Stagging the interface would be a nice idea.

--- src/TT.Domain/Services/IPrincipalAccessor.cs
+++ src/TT.Domain/Services/IPrincipalAccessor.cs
@@ -1,12 +1,34 @@
 ﻿using System.Collections.Generic;
+using System.Security.Claims;
 using System.Security.Principal;
 
 namespace TT.Domain.Services
 {
     public interface IPrincipalAccessor
     {
+        /// <summary>
+        /// Returns the principal.
+        /// </summary>
         IPrincipal RequestPrincipal { get; }
-        string RequestUserId { get; }
+
+        /// <summary>
+        /// Returns the user ID.
+        /// </summary>
+        string UserNameId { get; }
+
+        /// <summary>
+        /// Returns the user name.
+        /// </summary>
+        string UserName { get; }
+
+        /// <summary>
+        /// Returns the roles that the principal has.
+        /// </summary>
         IEnumerable<string> Roles { get; }
+
+        /// <summary>
+        /// Returns the claims that the principal.
+        /// </summary>
+        IEnumerable<Claim> Claims { get; }
     }
 }

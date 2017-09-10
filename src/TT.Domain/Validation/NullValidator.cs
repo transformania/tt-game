a7356496adf3e6ae33f224ca75b2e889f9599df0
Eric Force <augustustate94@gmail.com>
2017-09-10 18:07:11 +0000

    Resolve "Register only the validators that take a class implementing IRequest and use the interface IValidator as the service type."

---
+++ src/TT.Domain/Validation/NullValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using MediatR;
+
+namespace TT.Domain.Validation
+{
+    /// <summary>
+    /// An empty <see cref="IValidator{T}"/> used if an <see cref="IRequest"/> does not have an <see cref="IValidator{T}"/>.
+    /// </summary>
+    public class NullValidator : AbstractValidator<IRequest>
+    {
+    }
+
+    /// <summary>
+    /// An empty <see cref="IValidator{T}"/> used if an <see cref="IRequest{TResponse}"/> does not have an <see cref="IValidator{T}"/>.
+    /// </summary>
+    public class NullValidatorWithResponse<TResponse> : AbstractValidator<IRequest<TResponse>>
+    {
+    }
+}

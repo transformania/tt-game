69aacd8fda2b555c9d900b79a2e0f38e2f7d10b5
Arrhae Khellian <arrhae.khellian@gmail.com>
2017-09-16 19:24:13 +0000

    Friends list cleanup

--- src/TT.Domain/ViewModels/SetFriendNicknameViewModel.cs
+++ src/TT.Domain/ViewModels/SetFriendNicknameViewModel.cs
@@ -1,13 +1,7 @@
-﻿using TT.Domain.Models;
-
-namespace TT.Domain.ViewModels
+﻿namespace TT.Domain.ViewModels
 {
     public class SetFriendNicknameViewModel
     {
-        public Player Owner { get; set; }
-        public string OwnerMembershipId { get; set; }
-        public Player Friend { get; set; }
-        public string FriendMembershipId { get; set; }
         public string Nickname { get; set; }
         public int FriendshipId { get; set; }
     }

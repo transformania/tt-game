735b68600eb92cdf1faddd73ad190e639e167492
Eric Force <augustustate94@gmail.com>
2018-03-12 00:59:03 -0500

    Changed AutoMapper to be injected using Simple Injector. The TestBase also was given virtual methods for configuring the mapper. This allows profiles to request services that are registered and for the services to be mocked in testing.

--- src/TT.Tests/TestBase.cs
+++ src/TT.Tests/TestBase.cs
@@ -1,6 +1,11 @@
-﻿using Highway.Data;
+﻿using AutoMapper;
+using Highway.Data;
 using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
 using TT.Domain;
+using TT.Tests.Utilities;
 
 namespace TT.Tests
 {
@@ -9,7 +14,8 @@ namespace TT.Tests
     {
         protected IDataContext DataContext { get; private set; }
         protected IDomainRepository Repository { get; private set; }
-        
+        protected IMapper Mapper { get; private set; }
+
         [SetUp]
         public virtual void SetUp()
         {
@@ -17,7 +23,34 @@ namespace TT.Tests
             Repository = new DomainRepository(DataContext);
 
             DomainRegistry.Repository = Repository;
-            DomainRegistry.ConfigureMapper();
+
+            var mapper = GetMapper();
+            Mapper = mapper;
+            DomainRegistry.SetMapperFunc(() => mapper);
+        }
+
+        public virtual IMapper GetMapper()
+        {
+            return new MapBuilder()
+                .AddAssemblies(GetMapperAssemblies())
+                .AddProfileTypes(GetMapperProfileTyes())
+                .AddProfileInstances(GetMapperProfileInstances())
+                .BuildMapper();
+        }
+
+        public virtual IEnumerable<Assembly> GetMapperAssemblies()
+        {
+            return new List<Assembly> { typeof(DomainRegistry).Assembly };
+        }
+
+        public virtual IEnumerable<Type> GetMapperProfileTyes()
+        {
+            return new List<Type>() { };
+        }
+
+        public virtual IEnumerable<Profile> GetMapperProfileInstances()
+        {
+            return new List<Profile>() { };
         }
     }
 }

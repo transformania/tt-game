1d7e4798c6623fd9fe5d63e8cb1f2e9a5452fccb
JudooTT <maltorvha@gmail.com>
2018-05-07 17:11:56 -0400

    Adding a query that checks to see if a player can interact with a generic static NPC

---
+++ src/TT.Domain/Players/Queries/CanInteractWith.cs
@@ -0,0 +1,58 @@
+﻿using System.Linq;
+using Highway.Data;
+using TT.Domain.Exceptions;
+using TT.Domain.Players.Entities;
+using TT.Domain.Statics;
+
+namespace TT.Domain.Players.Queries
+{
+    public class CanInteractWith : DomainQuerySingle<bool>
+    {
+        public int BotId { get; set; }
+        public int PlayerId { get; set; }
+
+        public override bool Execute(IDataContext context)
+        {
+
+            ContextQuery = ctx =>
+            {
+                var npc = ctx.AsQueryable<Player>()
+                    .FirstOrDefault(m => m.BotId == BotId);
+
+                var player = ctx.AsQueryable<Player>()
+                    .FirstOrDefault(m => m.Id == PlayerId);
+
+                if (npc == null)
+                    throw new DomainException($"NPC with BotId '{BotId}' does not exist");
+
+                if (player == null)
+                    throw new DomainException($"Player with Id '{PlayerId}' does not exist");
+
+                var pronoun = npc.Gender == PvPStatics.GenderFemale ? "her" : "him";
+
+                if (player.Mobility != PvPStatics.MobilityFull)
+                    throw new DomainException($"You must be animate in order to interact with {npc.GetFullName()}.");
+
+                if (npc.Mobility != PvPStatics.MobilityFull)
+                    throw new DomainException($"{npc.GetFullName()} must be animate in order for you to interact with {pronoun}.");
+
+                if (player.InDuel > 0)
+                    throw new DomainException($"You must conclude your duel in order to interact with {npc.GetFullName()}.");
+
+                if (player.InQuest > 0)
+                    throw new DomainException($"You must conclude your quest in order to interact with {npc.GetFullName()}.");
+
+
+                if (player.Location != npc.Location)
+                {
+                   
+                    throw new DomainException($"You must be in the same location as {npc.GetFullName()} in order to interact with {pronoun}.");
+                }
+
+                return true;
+            };
+
+            return ExecuteInternal(context);
+        }
+    }
+}
---
+++ src/TT.Tests/Players/Queries/CanInteractWithTests.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using FluentAssertions;
+using NUnit.Framework;
+using TT.Domain.Exceptions;
+using TT.Domain.Players.Entities;
+using TT.Domain.Players.Queries;
+using TT.Domain.Statics;
+using TT.Tests.Builders.Players;
+
+namespace TT.Tests.Players.Queries
+{
+    [TestFixture]
+    public class CanInteractWithTests : TestBase
+    {
+        private Player npc;
+        private Player player;
+
+        [SetUp]
+        public void Init()
+        {
+            npc = new PlayerBuilder().With(p => p.BotId, -15)
+                .With(p => p.FirstName, "Bot")
+                .With(p => p.LastName, "Bottonson")
+                .With(p => p.Mobility, PvPStatics.MobilityFull)
+                .With(p => p.Location, LocationsStatics.STREET_130_SUNNYGLADE_DRIVE)
+                .BuildAndSave();
+
+            player = new PlayerBuilder().With(p => p.BotId, AIStatics.ActivePlayerBotId)
+                .With(p => p.Id, 1)
+                .With(p => p.FirstName, "Bob")
+                .With(p => p.LastName, "Human")
+                .With(p => p.Mobility, PvPStatics.MobilityFull)
+                .With(p => p.Location, LocationsStatics.STREET_130_SUNNYGLADE_DRIVE)
+                .BuildAndSave();
+        }
+
+        [Test]
+        public void Should_return_true_if_conditions_okay()
+        {
+            Repository.FindSingle(new CanInteractWith { BotId = npc.BotId, PlayerId = player.Id }).Should().Be(true);
+        }
+
+        [Test]
+        public void Should_throw_exception_if_npc_not_found()
+        {
+            var action = new Action(() => { Repository.FindSingle(new CanInteractWith { BotId = -99, PlayerId = player.Id}); });
+
+            action.Should().ThrowExactly<DomainException>().WithMessage("NPC with BotId '-99' does not exist");
+        }
+
+        [Test]
+        public void Should_throw_exception_if_player_not_found()
+        {
+            var action = new Action(() => { Repository.FindSingle(new CanInteractWith { BotId = npc.BotId, PlayerId = -99}); });
+
+            action.Should().ThrowExactly<DomainException>().WithMessage("Player with Id '-99' does not exist");
+        }
+
+        [Test]
+        public void Should_throw_exception_if_player_not_animate()
+        {
+            var inanimatePlayer = new PlayerBuilder().With(p => p.BotId, AIStatics.ActivePlayerBotId)
+                .With(p => p.Id, 2)
+                .With(p => p.FirstName, "Bob")
+                .With(p => p.LastName, "Human")
+                .With(p => p.Mobility, PvPStatics.MobilityInanimate)
+                .With(p => p.Location, LocationsStatics.STREET_130_SUNNYGLADE_DRIVE)
+                .BuildAndSave();
+
+            var action = new Action(() => { Repository.FindSingle(new CanInteractWith { BotId = npc.BotId, PlayerId = inanimatePlayer.Id }); });
+
+            action.Should().ThrowExactly<DomainException>().WithMessage("You must be animate in order to interact with Bot Bottonson.");
+        }
+
+        [Test]
+        public void Should_throw_exception_if_player_in_duel()
+        {
+            var duelPlayer = new PlayerBuilder().With(p => p.BotId, AIStatics.ActivePlayerBotId)
+                .With(p => p.Id, 100)
+                .With(p => p.FirstName, "Bob")
+                .With(p => p.LastName, "Human")
+                .With(p => p.InDuel, 50)
+                .BuildAndSave();
+
+            var action = new Action(() => { Repository.FindSingle(new CanInteractWith { BotId = npc.BotId, PlayerId = duelPlayer.Id }); });
+
+            action.Should().ThrowExactly<DomainException>().WithMessage("You must conclude your duel in order to interact with Bot Bottonson.");
+        }
+
+        [Test]
+        public void Should_throw_exception_if_player_in_quest()
+        {
+            var questPlayer = new PlayerBuilder().With(p => p.BotId, AIStatics.ActivePlayerBotId)
+                .With(p => p.Id, 500)
+                .With(p => p.FirstName, "Bob")
+                .With(p => p.LastName, "Human")
+                .With(p => p.InQuest, 50)
+                .BuildAndSave();
+
+            var action = new Action(() => { Repository.FindSingle(new CanInteractWith { BotId = npc.BotId, PlayerId = questPlayer.Id }); });
+
+            action.Should().ThrowExactly<DomainException>().WithMessage("You must conclude your quest in order to interact with Bot Bottonson.");
+        }
+
+        [Test]
+        public void Should_throw_exception_if_bot_not_animate()
+        {
+            var inanimateNPC = new PlayerBuilder().With(p => p.BotId, -16)
+                .With(p => p.Id, 123)
+                .With(p => p.FirstName, "Bot")
+                .With(p => p.LastName, "Bottonson")
+                .With(p => p.Mobility, PvPStatics.MobilityInanimate)
+                .With(p => p.Location, LocationsStatics.STREET_130_SUNNYGLADE_DRIVE)
+                .BuildAndSave();
+
+            var action = new Action(() => { Repository.FindSingle(new CanInteractWith { BotId = inanimateNPC.BotId, PlayerId = player.Id }); });
+
+            action.Should().ThrowExactly<DomainException>().WithMessage("Bot Bottonson must be animate in order for you to interact with him.");
+
+        }
+
+        [Test]
+        public void Should_throw_exception_if_wrong_location()
+        {
+            var wrongPlacePlayer = new PlayerBuilder().With(p => p.BotId, AIStatics.ActivePlayerBotId)
+                .With(p => p.Id, 35)
+                .With(p => p.FirstName, "Bob")
+                .With(p => p.LastName, "Human")
+                .With(p => p.Mobility, PvPStatics.MobilityFull)
+                .With(p => p.Location, LocationsStatics.STREET_270_WEST_9TH_AVE)
+                .BuildAndSave();
+
+            var action = new Action(() => { Repository.FindSingle(new CanInteractWith { BotId = npc.BotId, PlayerId = wrongPlacePlayer.Id }); });
+
+            action.Should().ThrowExactly<DomainException>().WithMessage("You must be in the same location as Bot Bottonson in order to interact with him.");
+        }
+
+    }
+}

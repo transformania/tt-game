7c6d3e935cf73838357783a5754320d36e7da416
Travis Timpson <5995173-tratim@users.noreply.gitlab.com>
2020-08-29 13:04:09 +0100

    Coven chatrooms, fixes #655

--- src/TT.Domain/ViewModels/ChatViewModel.cs
+++ src/TT.Domain/ViewModels/ChatViewModel.cs
@@ -2,6 +2,7 @@ namespace TT.Domain.ViewModels
 {
     public class ChatViewModel
     {
+        public string Room { get; set; }
         public string RoomName { get; set; }
         public string ChatUser { get; set; }
         public string ChatColor { get; set; }

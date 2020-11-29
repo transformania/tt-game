25909a05bd1317588d6df68d61d4962dc405fadb
abj <itokuzu@gmail.com>
2020-11-28 23:16:23 -0800

    Fixes #251. When any spell besides weaken is cast to completion, it should remove any stored-up self restore pool.

---
+++ src/TT.Domain/Legacy/Abstract/ISelfRestoreEnergies.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq;
+using TT.Domain.Models;
+
+namespace TT.Domain.Abstract
+{
+    public interface ISelfRestoreEnergies
+    {
+
+        IQueryable<Models.SelfRestoreEnergies> SelfRestoreEnergies { get; }
+
+        void DeleteSelfRestoreEnergies(int SelfRestoreEnergiesId);
+
+    }
+}
---
+++ src/TT.Domain/Legacy/Concrete/EFSelfRestoreEnergies.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+using TT.Domain.Abstract;
+using TT.Domain.Models;
+
+namespace TT.Domain.Concrete
+{
+    public class EFSelfRestoreEnergies : ISelfRestoreEnergies
+    {
+        private StatsContext context = new StatsContext();
+
+        public IQueryable<Models.SelfRestoreEnergies> SelfRestoreEnergies
+        {
+            get
+            { 
+                return context.SelfRestoreEnergies;
+            }
+        }
+
+        public void DeleteSelfRestoreEnergies(int id)
+        {
+            var dbEntry = context.SelfRestoreEnergies.Find(id);
+            if (dbEntry != null)
+            {
+                context.SelfRestoreEnergies.Remove(dbEntry);
+                context.SaveChanges();
+            }
+        }
+
+    }
+}

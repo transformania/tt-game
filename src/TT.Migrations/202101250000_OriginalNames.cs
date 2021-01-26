f14af340612f7c1ca901c691400adf83fd3a59f5
Travis Timpson <5995173-tratim@users.noreply.gitlab.com>
2021-01-26 01:27:30 +0000

    Add original player names columns to database.  Fixes #731

---
+++ src/TT.Migrations/202101250000_OriginalNames.cs
@@ -0,0 +1,19 @@
+﻿using FluentMigrator;
+
+namespace TT.Migrations
+{
+    [Migration(202101250000)]
+    public class OriginalNames : ForwardOnlyMigration
+    {
+        public override void Up()
+        {
+            Alter.Table("Players").AddColumn("OriginalFirstName").AsCustom("nvarchar(MAX)").Nullable();
+            Alter.Table("Players").AddColumn("OriginalLastName").AsCustom("nvarchar(MAX)").Nullable();
+
+            Execute.Sql("UPDATE Players SET OriginalFirstName = FirstName, OriginalLastName = LastName");
+
+            Alter.Table("Players").AlterColumn("OriginalFirstName").AsCustom("nvarchar(MAX)").NotNullable();
+            Alter.Table("Players").AlterColumn("OriginalLastName").AsCustom("nvarchar(MAX)").NotNullable();
+        }
+    }
+}

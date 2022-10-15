5edbce14ac409c41212d854400457cc94eef1c59
abj <itokuzu@gmail.com>
2022-10-14 17:21:16 -0700

    Adding a unit test for IsChaosChangesEnabled query.

---
+++ src/TT.Tests/Identity/Queries/IsChaosChangesEnabledTests.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+using TT.Domain;
+using TT.Domain.Exceptions;
+using TT.Domain.Identity.Queries;
+using TT.Tests.Builders.Identity;
+
+namespace TT.Tests.Identity.Queries
+{
+    [TestFixture]
+    public class IsChaosChangesEnabledTests : TestBase
+    {
+        [Test]
+        public void return_true_chaos_enabled()
+        {
+            var user = new UserBuilder()
+                .With(u => u.AllowChaosChanges, true)
+                .BuildAndSave();
+
+            Assert.That(DomainRegistry.Repository.FindSingle(new IsChaosChangesEnabled { UserId = user.Id }), Is.True);
+        }
+
+        [Test]
+        public void return_false_chaos_enabled()
+        {
+            var user = new UserBuilder()
+                .With(u => u.AllowChaosChanges, false)
+                .BuildAndSave();
+
+            Assert.That(DomainRegistry.Repository.FindSingle(new IsChaosChangesEnabled { UserId = user.Id }), Is.False);
+        }
+
+        [Test]
+        public void throw_exception_if_userid_doesnt_exist()
+        {
+            var cmd = new IsChaosChangesEnabled { UserId = "abcde" };
+            Assert.That(() => Repository.FindSingle(cmd),
+                Throws.TypeOf<DomainException>().With.Message.EqualTo("User with Id abcde does not exist"));
+        }
+    }
+}

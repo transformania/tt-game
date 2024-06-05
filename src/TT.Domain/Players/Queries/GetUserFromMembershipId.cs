b52b533a85f131e537746a311fed8db4f7399c82
Flynn <dagtables@gmail.com>
2024-06-05 20:33:46 +0000

    Bugfix for Moderators Viewing NPC Player Pages

---
+++ src/TT.Domain/Players/Queries/GetUserFromMembershipId.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+using Highway.Data;
+using TT.Domain.Identity.Entities;
+
+namespace TT.Domain.Players.Queries
+{
+    public class GetUserFromMembershipId : DomainQuerySingle<User>
+    {
+        public string UserId { get; set; }
+
+        public override User Execute(IDataContext context)
+        {
+            ContextQuery = ctx =>
+            {
+                return ctx.AsQueryable<User>()
+                    .SingleOrDefault(p => p.Id == UserId);
+            };
+
+            return ExecuteInternal(context);
+        }
+    }
+}
---
+++ src/TT.Tests/Players/Queries/GetUserFromMembershipIdTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using TT.Domain;
+using TT.Domain.Players.Queries;
+using TT.Tests.Builders.Identity;
+
+namespace TT.Tests.Players.Queries
+{
+    [TestFixture]
+    public class GetUserFromMembershipIdTests : TestBase
+    {
+        [Test]
+        public void Should_not_return_null()
+        {
+            var user = new UserBuilder()
+                .With(p => p.Id, "guid")
+                .BuildAndSave();
+
+            var cmd = new GetUserFromMembershipId { UserId = user.Id };
+            Assert.IsNotNull(DomainRegistry.Repository.FindSingle(cmd));
+        }
+    }
+}
